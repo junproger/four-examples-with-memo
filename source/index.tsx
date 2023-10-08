811ea1a82f526ad4b98ee9e00569903263e1a6c1
Junior Programmer <progphyton@gmail.com>
2023-10-08 09:08:11 +0300

    refactor: project files

--- source/index.tsx
+++ source/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 import { container } from './constants/container';
 
-import { App } from './components/app/app';
+import { App } from './components/App';
 
 const ROOT = createRoot(container as HTMLElement);
 
